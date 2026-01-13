c7fb039d5caac4bbee45274471b543220930ae96
maheshwar10-05 <maheshwar100501@gmail.com>
2026-01-13 22:16:55 +0530

    added os

--- playwrightTestingPractice/Utilities/utility.py
+++ playwrightTestingPractice/Utilities/utility.py
@@ -1,4 +1,19 @@
 import json
+import os
+
+# 1. Get the directory where utility.py is located
+current_dir = os.path.dirname(__file__)
+
+# 2. Construct the path to the JSON file dynamically
+# This goes up one level from 'Utilities' then into 'Testdata'
+json_path = os.path.join(current_dir, "..", "Testdata", "registerdetails.json")
+
+# 3. Open the file using the dynamic path
+with open(json_path) as file:
+    # ... your existing code ...
+    user_login2=json.load(file)
+    user_credentials2 = user_login2["user_details"]
+    print(user_credentials2)
 
 with open("../Testdata/registerdetails.json") as file:
     user_data = json.load(file)
